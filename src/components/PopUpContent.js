b66118bb9077782c0f07f45a6a8d131cd9de90e7
Marco Lölkes <marcoloelkes@gmx.de>
2023-10-01 23:40:10 +0200

    Make whole popup clickable

--- src/components/PopUpContent.js
+++ src/components/PopUpContent.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 function PopUpContent({ ideaData }) {
 
     return (
-        <div className="PopUpContent" style={{ backgroundColor: ideaData.color, color: ideaData.fontColor }}>
-            <p className="snippet">„{ideaData.snippet}“</p>
-            <Link className="link" to={`/ideas/${ideaData.id}`} style={{ color: ideaData.fontColor }}>{ideaData.title}</Link>
+        <div className="PopUpContent" style={{ backgroundColor: ideaData.color }}>
+            <Link className="link" to={`/ideas/${ideaData.id}`}>
+                <p className="snippet" style={{ color: ideaData.fontColor }}>„{ideaData.snippet}“</p>
+                <p className="snippet-info" style={{ color: ideaData.fontColor }}>{ideaData.title}</p>
+            </Link>
         </div>
     )
 }

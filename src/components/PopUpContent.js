e5bed8b9c6ed1258ff2159d69bde0c225d576327
Marco Lölkes <marcoloelkes@gmx.de>
2023-10-01 22:36:33 +0200

    Add font color for articles and use it in popups

--- src/components/PopUpContent.js
+++ src/components/PopUpContent.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function PopUpContent({ ideaData }) {
 
     return (
-        <div className="PopUpContent" style={{ backgroundColor: ideaData.color }}>
+        <div className="PopUpContent" style={{ backgroundColor: ideaData.color, color: ideaData.fontColor }}>
             <p className="snippet">„{ideaData.snippet}“</p>
-            <Link className="link" to={`/ideas/${ideaData.id}`}>{ideaData.title}</Link>
+            <Link className="link" to={`/ideas/${ideaData.id}`} style={{ color: ideaData.fontColor }}>{ideaData.title}</Link>
         </div>
     )
 }

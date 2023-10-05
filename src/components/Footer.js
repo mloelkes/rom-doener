6becdeeaf8d366b9abd2e0b92f18c650dc316301
Marco Lölkes <marcoloelkes@gmx.de>
2023-10-05 15:19:51 +0200

    Add underline effect for navigation and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Footer({backgroundStyle}) {
 
     return (
         <footer style={backgroundStyle}>
-            <span><Link to="/privacy">Datenschutz</Link><Link to="/imprint">Impressum</Link></span>
+            <span><NavLink to="/privacy" className={({isActive}) => (isActive ? "link selected" : "link")}>Datenschutz</NavLink><NavLink to="/imprint" className={({isActive}) => (isActive ? "link selected" : "link")}>Impressum</NavLink></span>
         </footer>
     )
 }

144d09a5775f808c25b3369d6ba9f2ce687d9d63
Marco Lölkes <marcoloelkes@gmx.de>
2023-11-12 18:56:12 +0100

    Navigation: Show navlink as active on homepage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
-function Navigation(props) { 
+function Navigation() {
+    const location = useLocation();
+
     return (
         <div className="Navigation">
             <div className="links">
-                <NavLink className={({isActive}) => (isActive ? "navlink selected" : "navlink")} to="/ideas">Ideen</NavLink>
+                <NavLink className={["/registry", "/discover", "/about"].includes(location.pathname) ? "navlink" : "navlink selected"} to="/ideas">Ideen</NavLink>
                 <NavLink className={({isActive}) => (isActive ? "navlink selected" : "navlink")} to="/registry">Register</NavLink>
                 <NavLink className={({isActive}) => (isActive ? "navlink selected" : "navlink")} to="/discover">Karte</NavLink>
                 <NavLink className={({isActive}) => (isActive ? "navlink selected" : "navlink")} to="/about">Über</NavLink>
@@ -13,7 +15,7 @@ function Navigation(props) {
 
             {/* <Link className="link home" to="/">Rom/Döner</Link> */}
             <Link className="home" to="/"><img src={process.env.PUBLIC_URL + "/images/logo.png"}  alt="logo" /></Link>
-        </div>
+        </div>  
     )
 }
 

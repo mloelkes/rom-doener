2fa753cf048ea65ca15352cd3974963a3a09fd2f
Marco Lölkes <marcoloelkes@gmx.de>
2023-11-16 22:51:32 +0100

    Bug: Ideas page - On hover white line is displayed on bigger displays

--- src/components/IdeasRow.js
+++ src/components/IdeasRow.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function IdeasRow({ideasEntry}) {
     return (
-        <div className="IdeasRow">
-            <Link to={`/ideas/${ideasEntry.id}`} className="link" style={{ backgroundColor: ideasEntry?.color }}>
+        <div className="IdeasRow" style={{ backgroundColor: ideasEntry?.color }}>
+            <Link to={`/ideas/${ideasEntry.id}`} className="link">
                 <h3 className="tag">{ideasEntry?.tag}</h3>
                 <p className="title">{ideasEntry?.title}</p>
             </Link>

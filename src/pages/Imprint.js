5f7756b649af7954e175abc20466348ef53312bc
Marco Lölkes <marcoloelkes@gmx.de>
2024-02-29 20:20:02 +0100

    Update content of Imprint page

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -25,6 +25,9 @@ function Imprint() {
                 <h2>Illustration</h2>
                 <p>Fabia Matveev (<a href="https://www.fabiafabia.com">fabiafabia.com</a>)</p>
                 <br/>
+                <h2>Lektorat</h2>
+                <p>Ferdinand Gutemann</p>
+                <br/>
                 <h2>Technische Umsetzung</h2>
                 <p>Marco Lölkes</p>
                 <br/>
@@ -35,7 +38,7 @@ function Imprint() {
                 <p>Kenneth Hujer<br/>Freiligrathstraße 56<br/>60385 Frankfurt am Main<br/>Telefon: <a href="tel:+4915753433551">0157 534 335 51</a><br/>E-Mail: <a href="mailto:kennethhujer@gmail.com">kennethhujer@gmail.com</a></p>
                 <p>Inhaltlich verantwortlich für diese Webseite i.S.d. § 55 II RStV: Kenneth Hujer</p>
                 <br/>
-                <p>© 2023 Kenneth Hujer</p>
+                <p>© 2024 Kenneth Hujer</p>
             </div>
             <Footer />
         </div>
